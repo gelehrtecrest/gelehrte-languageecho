14fc50959b5d4cca53f82a21776c43134f6dee9d
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2022-04-20 00:08:03 +0900

    教師ありデータのうち7割学習に、3割正解にコピーする

---
+++ train-data.sh
@@ -0,0 +1,31 @@
+# すでに学習済みの場合は削除する
+if [ -e ./models/research/saved_model_01 ]; then
+    # 存在する場合
+	rm -rf ./models/research/saved_model_01
+fi
+
+# 保存先のデータの保管
+dir="./models/research/object_detection_tools/data/"
+train_dir=$dir"train/"
+val_dir=$dir"val/"
+
+echo $train_dir > test.txt
+echo $val_dir > test2.txt
+
+# 保存済みのデータの削除
+rm $train_dir*.tfrecord
+rm $val_dir*.tfrecord
+
+
+for file in `\find ./image/tags/languageecho-TFRecords-export -maxdepth 1 -type f -name "*.tfrecord"` ; do
+    if [[ $(($RANDOM % 10)) -lt 7 ]] ; then
+	    cp $file $train_dir
+	else
+		cp $file $val_dir
+	fi
+done
+
+cd ./models/research/object_detection_tools/data
+./change_tfrecord_filename.sh
+
+cd ../../../../
