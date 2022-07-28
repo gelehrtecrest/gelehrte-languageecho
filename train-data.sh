ad47196599ea82a7c119563cb2c01abf26529526
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2022-07-28 11:52:33 +0900

    tfrecordを保存

--- train-data.sh
+++ train-data.sh
@@ -9,12 +9,19 @@ dir="./models/research/object_detection_tools_gelehrte/data/"
 train_dir=$dir"train/"
 val_dir=$dir"val/"
 
+# 公開用フォルダ
+open_dir="../languageecho-tfrecord/eorzea/"
+
 # 保存済みのデータの削除
 rm $train_dir*.tfrecord
 rm $val_dir*.tfrecord
 
+# 公開用フォルダのtfrecordの削除
+rm $open_dir*.tfrecord
+
 
 for file in `\find ./image/tags/languageecho-TFRecords-export -maxdepth 1 -type f -name "*.tfrecord"` ; do
+	cp $file $open_dir
     if [[ $(($RANDOM % 10)) -lt 7 ]] ; then
 	    cp $file $train_dir
 	else
